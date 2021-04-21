7fb9b40acaa842dbaeb469dc7bd04c1f14cb929c
safarsafarov <safarov.safar@hotmail.com>
2021-04-21 09:45:04 +0800

    commit

--- Views/ContentView.swift
+++ Views/ContentView.swift
@@ -13,6 +13,9 @@ struct ContentView : View {
                     TourTitle(title: "Savanna Trek", caption: "15 mile drive followed by an hour long trek")
                         .offset(x: 0, y: -15)
                         .padding(.leading, 30)
+                        .offset(x: self.zoomed ? 500 : 0, y: -15)
+                        .animation(.default)
+
                     
                     Spacer()
                 }
@@ -21,11 +24,12 @@ struct ContentView : View {
                     .position(x: self.zoomed ? 220 : 600, y: 50)
                     .scaleEffect(0.33)
                     .shadow(radius: 10)
+                    .animation(.spring())
                     .onTapGesture {
                         self.zoomed.toggle()
                     }
                     .scaleEffect(self.zoomed ? 1.33 : 0.33)
-                    .animation(.spring())
+                    
                 
             }
             .background(Color(red: 0.1, green: 0.1, blue: 0.1))
