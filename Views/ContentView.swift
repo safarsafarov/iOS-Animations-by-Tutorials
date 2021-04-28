426a244f8a324989e093faf19f17542583e30d47
safarsafarov <safarov.safar@hotmail.com>
2021-04-28 10:58:39 +0800

    commit

--- Views/ContentView.swift
+++ Views/ContentView.swift
@@ -8,27 +8,29 @@ struct ContentView : View {
         VStack(spacing: 0) {
             HeroImage(name: "hero")
             
-            GeometryReader() { geometry in
-                ZStack {
-                    HStack {
-                        TourTitle(title: "Savanna Trek", caption: "15 mile drive followed by an hour long trek")
-                            .offset(x: 0, y: -10)
-                            .padding(.leading,30)
-                            .offset(x: self.zoomed ? 500 : 0, y: -15)
-                            .animation(.default)
+            
+            ZStack {
+              
+                HStack {
+                    TourTitle(title: "Savanna Trek", caption: "15 mile drive followed by an hour long trek")
+                        .offset(x: 0, y: -10)
+                        .padding(.leading,30)
+                        .offset(x: self.zoomed ? 500 : 0, y: -15)
+                        .animation(.default)
 
-                        Spacer()
-                    }
-                    
+                    Spacer()
+                }
+                GeometryReader() { geometry in
                     Image("thumb")
-                        .rotationEffect(.degrees(90))
+//                        .rotationEffect(.degrees(90))
                         .clipShape(RoundedRectangle(cornerRadius: self.zoomed ? 40 : 500 ))
                         .overlay(
                             Circle()
                                 .fill(self.zoomed ? Color.clear : Color(red: 1.0, green: 1.0, blue: 1.0, opacity: 0.4))
                                 .scaleEffect(0.8)
                         )
-                        .position(x: self.zoomed ? 220 : 600, y: 50)
+                        .position(x: self.zoomed ? geometry.frame(in: .local).midX : 600,
+                          y: 60)
                         .shadow(radius: 10)
                         .animation(.spring())
                         .onTapGesture {
@@ -36,11 +38,11 @@ struct ContentView : View {
                             
                         }
                         .scaleEffect(self.zoomed ? 1.33 : 0.33)
-                    
-                    
                 }
+                
+                
+                
             }
-         
             .background(Color(red: 0.1, green: 0.1, blue: 0.1))
             
             
